69e8fc72ac1ccaa13e441d66a130d485e70bc090
David Bryant <nedink@gmail.com>
2022-12-09 12:54:54 -0500

    try to help vercel preview deployments

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -13,6 +13,7 @@ const getBaseUrl = () => {
   // useEffect(() => setColor('red'), [])
 
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
+  if (process.env.NEXT_PUBLIC_VERCEL_URL) return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`; // try this too
   return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
 };
 
