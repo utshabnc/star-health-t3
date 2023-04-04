2fd70a0e8e6b88def4eaae31fb2b6e30d9e152d7
Abraham Perez <aperezpe01@gmail.com>
2023-04-04 10:11:04 -0500

    Route sign in button directly to Google

--- src/components/PayWall/PayWall.tsx
+++ src/components/PayWall/PayWall.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export const PayWall = () => {
   const session = useSession();
 
-  if (!session?.data?.user) {
+  if (session?.data?.user) {
     return (<div className="absolute bottom-0 left-0 w-full h-full backdrop-blur-sm flex justify-center items-center">
       {/* Sign in block */}
       <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4 shadow-lg rounded-lg border">
@@ -23,7 +23,7 @@ export const PayWall = () => {
         </div>
 
         <div className="mt-8">
-          <button onClick={() => signIn()} className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out">
+          <button onClick={() => signIn("google")} className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out">
             <span className="absolute right-0 inset-y-0 flex items-center pr-3">
               <svg className="h-6 w-6 text-indigo-500 group-hover:text-indigo-400 transition ease-in-out duration-150" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 5l7 7-7 7M5 5l7 7-7 7" />
