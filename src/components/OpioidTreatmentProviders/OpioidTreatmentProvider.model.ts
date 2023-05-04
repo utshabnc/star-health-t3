3867a18827ad1c6648b645f0008be70cdb2b3c57
Ilya Subkhankulov <ilya.subkhankulov@gmail.com>
2023-05-03 22:46:41 -0700

    removed old code

--- src/components/OpioidTreatmentProviders/OpioidTreatmentProvider.model.ts
+++ src/components/OpioidTreatmentProviders/OpioidTreatmentProvider.model.ts
@@ -10,29 +10,3 @@ export interface OpioidTreatmentProvider {
   medicare_id_effective_date?:  Date | null;
   phone?:                     string | null;
 }
-
-// export interface OpioidTreatmentProvider {
-//     id?:                        string | undefined;
-//     npi?:                       string | undefined;
-//     provider_name?:             string | undefined;
-//     address_line_1?:            string | undefined;
-//     address_line_2?:            string | null | undefined;
-//     city?:                      string | undefined;
-//     state?:                     string | undefined;
-//     zip?:                       string | undefined;
-//     medicare_id_effective_date?:  Date | null | undefined;
-//     phone?:                     string | null | undefined;
-//   }
-
-//   const data: {
-//     state?: string | undefined;
-//     id?: string | undefined;
-//     city?: string | undefined;
-//     npi?: string | undefined;
-//     provider_name?: string | undefined;
-//     address_line_1?: string | undefined;
-//     address_line_2?: string | ... 1 more ... | undefined;
-//     zip?: string | undefined;
-//     medicare_id_effective_date?: Date | ... 1 more ... | undefined;
-//     phone?: string | ... 1 more ... | undefined;
-// } | undefined
