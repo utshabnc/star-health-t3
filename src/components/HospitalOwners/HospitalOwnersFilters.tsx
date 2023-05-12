a47345e6e16f48731100003cdded39581562f9f5
Michael Pecoul <harddrive225@gmail.com>
2023-05-12 07:12:40 -0500

    created table for owner data

---
+++ src/components/HospitalOwners/HospitalOwnersFilters.tsx
@@ -0,0 +1,83 @@
+import { Dispatch, SetStateAction, useMemo} from "react";
+import { useEffect, useState } from "react";
+import type { HospitalOwners } from "./HospitalOwners.model";
+import type { Owners } from "./Owners.model";
+import Image from "next/image";
+import getData, {data} from "./processJSON"
+
+import cbi from '../../assets/logos/community-benefits.png';
+import { match } from "assert";
+
+interface HospitalOwnersFiltersProps {
+  params: {
+    hospitalOwnersData?: HospitalOwners[];
+    setHospitalOwnersData: Dispatch<SetStateAction<HospitalOwners[]>>;
+    setIsApiProcessing: Dispatch<SetStateAction<boolean>>;
+  }
+}
+
+export default function HospitalsFilters({params}: HospitalOwnersFiltersProps) {
+  const { hospitalOwnersData, setHospitalOwnersData, setIsApiProcessing } = params;
+  const [searchStr, setSearchStr] = useState("");
+  const [dataIsAvailable, setDataIsAvailable] = useState(false);
+  const [error, setError] = useState<any>("");
+
+  useEffect(() => {
+    if (hospitalOwnersData && hospitalOwnersData.length > 1) {
+      setDataIsAvailable(true);
+    }
+  }, [hospitalOwnersData])
+
+  useEffect(() => {
+    if (searchStr) {
+      const delayDebounceFn = setTimeout(() => {
+        setIsApiProcessing(true);
+
+        let matchedHospitals: HospitalOwners[] = [];
+        for (let i=0; i<data.length; i+=1) {
+          // console.log(JSON.stringify(data[i]).includes(searchStr.toUpperCase()))
+          if (JSON.stringify(data[i]).includes(searchStr.toUpperCase())) {
+            matchedHospitals.push(data[i])
+          }
+        }
+  
+        setHospitalOwnersData(matchedHospitals);
+        setIsApiProcessing(false);
+
+      }, 3000)
+
+      return () => clearTimeout(delayDebounceFn)
+    }
+  }, [searchStr])
+
+  
+
+  return (
+    <>
+      <div className="w-full">
+        <div>
+          <p className="p-1 text-xs font-semibold text-violet-900">
+            Search for hospitals
+          </p>
+          <div className="flex justify-between w-[100%] items-center gap-3">
+            <input
+              type="text"
+              placeholder="Search"
+              className="mx-1 my-2 w-[30%] cursor-pointer rounded-lg border border-violet-900 bg-violet-100 p-1 text-slate-900 placeholder:text-violet-800 hover:bg-violet-300 hover:text-violet-900"
+              value={searchStr}
+              onChange={(e) => setSearchStr(e.target.value)}
+            />
+
+            <Image
+              src={cbi}
+              alt=""
+              width={128}
+              height={128}
+              className="bottom-0 right-0 object-contain"
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ src/components/HospitalOwners/processJSON.tsx
@@ -0,0 +1,97 @@
+import {HospitalOwners} from "./HospitalOwners.model";
+import {Owners} from "./Owners.model";
+import { capitalizeWords } from "../../utils";
+
+import * as https from 'https';
+
+const url = "https://data.cms.gov/data-api/v1/dataset/029c119f-f79c-49be-9100-344d31d10344/data";
+
+export let data: any;
+
+export default async function getData() {
+    data=""
+    let jsonObj
+    await https.get(url, (res: any) => {
+        let body = "";
+        res.on("data", (chunk: any) => {
+            body += chunk;
+        });
+
+        res.on("end", () => {
+            try {
+                jsonObj = JSON.parse(body);
+                jsonObj = processJSONObject(jsonObj);
+                data = jsonObj;
+
+            } catch (error: any) {
+                console.error(error.message);
+            }
+        });
+
+        }).on("error", (error: any) => {
+            console.error(error.message);
+        });
+}
+
+const restructuredData: any = [];
+
+function processJSONObject(data: any) {
+    const hospitalNames: any = []
+    for (let i=0; i<data.length; i+=1) {
+        if (hospitalNames.includes(data[i]["ORGANIZATION NAME"])) {
+            continue;
+        }
+        hospitalNames.push(data[i]["ORGANIZATION NAME"]);
+    }
+
+    for (let x=0; x<hospitalNames.length; x+=1) {
+        const owners = [];
+        for (let i=0; i<data.length; i+=1) {
+            if (data[i]["ORGANIZATION NAME"] === hospitalNames[x]) {
+                owners.push({
+                    "ASSOCIATE_ID_OWNER": data[i]["ASSOCIATE ID - OWNER"],
+                    "TYPE_OWNER": data[i]["TYPE - OWNER"],
+                    "ROLE_CODE_OWNER": data[i]["ROLE CODE - OWNER"],
+                    "ROLE_TEXT_OWNER": capitalizeWords(data[i]["ROLE TEXT - OWNER"]),
+                    "ASSOCIATION_DATE_OWNER": data[i]["ASSOCIATION DATE - OWNER"],
+                    "NAME_OWNER": capitalizeWords(data[i]["FIRST NAME - OWNER"]+" "+data[i]["MIDDLE NAME - OWNER"]+" "+data[i]["LAST NAME - OWNER"]),
+                    "TITLE_OWNER": capitalizeWords(data[i]["TITLE - OWNER"]),
+                    "ORGANIZATION_NAME_OWNER": capitalizeWords(data[i]["ORGANIZATION NAME - OWNER"]),
+                    "DOING_BUSINESS_AS_NAME_OWNER": capitalizeWords(data[i]["DOING BUSINESS AS NAME - OWNER"]),
+                    "ADDRESS": data[i]["ADDRESS LINE 1 - OWNER"] == "" ? "" : capitalizeWords(data[i]["ADDRESS LINE 1 - OWNER"]+", "+data[i]["ADDRESS LINE 2 - OWNER"]+", "+data[i]["CITY - OWNER"]+", "+data[i]["STATE - OWNER"]+", "+data[i]["ZIP CODE - OWNER"]),
+                    "PERCENTAGE_OWNERSHIP": data[i]["PERCENTAGE OWNERSHIP"],
+                    "CREATED_FOR_ACQUISITION_OWNER": data[i]["CREATED FOR ACQUISITION - OWNER"],
+                    "CORPORATION_OWNER": data[i]["CORPORATION - OWNER"],
+                    "LLC_OWNER": data[i]["LLC - OWNER"],
+                    "MEDICAL_PROVIDER_SUPPLIER_OWNER": data[i]["MEDICAL PROVIDER SUPPLIER - OWNER"],
+                    "MANAGEMENT_SERVICES_COMPANY_OWNER": data[i]["MANAGEMENT SERVICES COMPANY - OWNER"],
+                    "MEDICAL_STAFFING_COMPANY_OWNER": data[i]["MEDICAL STAFFING COMPANY - OWNER"],
+                    "HOLDING_COMPANY_OWNER": data[i]["HOLDING COMPANY - OWNER"],
+                    "INVESTMENT_FIRM_OWNER": data[i]["INVESTMENT FIRM - OWNER"],
+                    "FINANCIAL_INSTITUTION_OWNER": data[i]["FINANCIAL INSTITUTION - OWNER"],
+                    "CONSULTING_FIRM_OWNER": data[i]["CONSULTING FIRM - OWNER"],
+                    "FOR_PROFIT_OWNER": data[i]["FOR PROFIT - OWNER"],
+                    "NON_PROFIT_OWNER": data[i]["NON PROFIT - OWNER"],
+                    "OTHER_TYPE_OWNER": data[i]["OTHER TYPE - OWNER"],
+                    "OTHER_TYPE_TEXT_OWNER": data[i]["OTHER TYPE TEXT - OWNER"]
+                });
+            }
+        }
+
+        for (let i=0; i<data.length; i+=1) {
+            if (data[i]["ORGANIZATION NAME"] === hospitalNames[x]) {
+                restructuredData.push({
+                    "ENROLLMENT_ID": data[i]["ENROLLMENT ID"],
+                    "ASSOCIATE_ID": data[i]["ASSOCIATE ID"],
+                    "ORGANIZATION_NAME": capitalizeWords(data[i]["ORGANIZATION NAME"]),
+                    "OWNERS": owners
+                });
+                break;
+            }
+        }
+
+    }
+    return restructuredData;
+}
+
+console.log("data: "+getData())
