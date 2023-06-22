34d0f018a3391b75d4e3beafbe03899dbc07aed3
Will Robertson <wrobe0709@gmail.com>
2023-06-22 16:14:00 -0500

    adding profile page work

---
+++ src/components/Bookmark/index.tsx
@@ -0,0 +1,72 @@
+import React, { useState } from 'react';
+import { TrashIcon } from '@heroicons/react/solid';
+
+interface BookmarkProps {
+  id: number;
+  title: string;
+  url: string;
+  notes: string;
+  date: string;
+  onDelete: (id: number) => void;
+}
+
+const Bookmarks: React.FC<BookmarkProps> = ({ id, title, url, notes, date, onDelete }) => {
+  const [editableNotes, setEditableNotes] = useState(notes);
+  const [showConfirmationModal, setShowConfirmationModal] = useState(false);
+
+  const handleNotesChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setEditableNotes(event.target.value);
+  };
+
+  const handleDeleteClick = () => {
+    setShowConfirmationModal(true);
+  };
+
+  const handleConfirmDelete = () => {
+    onDelete(id);
+    setShowConfirmationModal(false);
+  };
+
+  const handleCancelDelete = () => {
+    setShowConfirmationModal(false);
+  };
+
+  return (
+    <div className="bg-white shadow-lg rounded-lg p-4">
+      <div className="flex items-center justify-between mb-1">
+        <a href={url} target="_blank" rel="noopener noreferrer" className="text-xl font-bold">
+          {title}
+        </a>
+        <button onClick={handleDeleteClick} className="text-red-500">
+          <TrashIcon className="w-6 h-6" />
+        </button>
+      </div>
+      <h4 className="text-lg text-gray-500 mb-2">{`Bookmarked on ${date}`}</h4>
+      <h3 className="text-lg font-medium mb-2">Notes</h3>
+      <textarea
+        value={editableNotes}
+        onChange={handleNotesChange}
+        placeholder="Enter notes..."
+        rows={4}
+        className="w-full resize-none border rounded-md mb-4"
+      />
+      {showConfirmationModal && (
+        <div className="fixed inset-0 flex items-center justify-center bg-gray-500 bg-opacity-50">
+          <div className="bg-white p-4 rounded-md">
+            <p>Are you sure you want to delete this bookmark?</p>
+            <div className="flex justify-end mt-4">
+              <button onClick={handleCancelDelete} className="mr-2">
+                Cancel
+              </button>
+              <button onClick={handleConfirmDelete} className="bg-red-500 text-white rounded p-2">
+                Delete
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Bookmarks;
--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,49 +1,106 @@
 import Image from "next/image";
 import { useSession } from 'next-auth/react';
+import Bookmark from "../../components/Bookmark";
+import { useCallback, useEffect, useState } from "react";
+import Dropdown from "../../components/Dropdown";
 
-interface Bookmark {
+interface BookmarkInterface {
   id: number;
   title: string;
   url: string;
+  notes: string;
+  date: string;
 }
 
-interface ProfileProps {
-  photoUrl: string;
-  firstName: string;
-  lastName: string;
-  bookmarks: Bookmark[];
-}
-
-const ProfilePage: React.FC<ProfileProps> = ({ }) => {
+const ProfilePage: React.FC = () => {
   const { data: session, status } = useSession();
-  const bookmarks: Bookmark[] = [];
+  const bookmarks: BookmarkInterface[] = [
+    {
+      id: 1,
+      title: 'Effectiveness of Teleconsultation in Referring a Patient With Early Myocardial Infarction From Peripheral Hospital to Cardiac Centre in Hail, Saudi Arabia',
+      url: 'starhealth.io/clinicalTrial/someId',
+      notes: 'Interesting study about teleconsultation',
+      date: '1/1/2010'
+    }
+  ];
+
+  enum Filter {
+    ClinicalTrials = 'Clinical Trials',
+    Diseases = 'Diseases',
+    Doctors = 'Doctors',
+    Drugs = 'Drugs',
+    Food = 'Food',
+    Genetics = 'Genetics',
+    Hospitals = 'Hospitals',
+    HospitalOwners = 'Hospital Owners',
+    Insurance = 'Insurance',
+    Manufacturers = 'Manufacturers',
+    MedicalDevices = 'Medical Devices',
+    OpioidTreatment = 'Opioid Treatment',
+    Transactions = 'Transactions'
+  }
   const name = session?.user?.name || '';
+
+  const userPhoto = session?.user?.image || null;
+
+  const [selectedFilter, setSelectedFilter] = useState<Filter>(Filter.ClinicalTrials);
+  const [filteredBookmarks, setFilteredBookmarks] = useState<BookmarkInterface[]>(bookmarks);
+
+  const filterBookmarks = useCallback(() => {
+    setFilteredBookmarks(filteredBookmarks);
+  }, [filteredBookmarks]);
+
+  useEffect(() => {
+    filterBookmarks();
+  }, [selectedFilter, filterBookmarks]);
+
+
+
+  const handleFilterChange = (value: string | undefined) => {
+    setSelectedFilter(value as Filter);
+  };
 
   if (status === 'loading') {
     return <div>Loading...</div>;
   }
 
-  const userPhoto = session?.user?.image || null;
-
   return (
-    <div className="profile">
-      {userPhoto && (
-        <Image
-          src={userPhoto}
-          alt={'Profile'}
+    <div className="p-16">
+      <div className="flex items-center mb-4">
+        {userPhoto && (
+          <Image
+            className="rounded-full mr-4"
+            src={userPhoto}
+            alt={'Profile'}
+            width={128}
+            height={128}
+          />
+        )}
+        <h1 className="text-xl font-bold">{name}</h1>
+      </div>
+      <hr className="my-4" />
+      <div>
+        <Dropdown
+          items={Object.values(Filter).map(filter => ({ value: filter, label: filter }))}
+          onChange={handleFilterChange}
+          label={'Category'}
+          placeholder={'-'}
+          value={selectedFilter}
         />
-      )}
-      <h1>{name}</h1>
-      <h2>Bookmarks:</h2>
-      <ul>
-        {bookmarks.map(bookmark => (
-          <li key={bookmark.id}>
-            <a href={bookmark.url} target="_blank" rel="noopener noreferrer">
-              {bookmark.title}
-            </a>
-          </li>
+        <h2 className="text-lg font-semibold mb-2">Bookmarks:</h2>
+        {filteredBookmarks.map(bookmark => (
+          <Bookmark
+            key={bookmark.id}
+            date={bookmark.date}
+            id={bookmark.id}
+            notes={bookmark.notes}
+            title={bookmark.title}
+            url={bookmark.url}
+            onDelete={() => { console.log('I clicked delete oh no') }}
+          />
         ))}
-      </ul>
+
+      </div>
     </div>
   );
 };
