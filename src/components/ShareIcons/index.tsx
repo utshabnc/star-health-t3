2ede6b10870d3d4bcc3cf38e8f443a3aaf531cde
Samuel Peppard <samuel_peppard@comcast.com>
2023-07-24 14:34:23 -0400

    add dynamic QR code

--- src/components/ShareIcons/index.tsx
+++ src/components/ShareIcons/index.tsx
@@ -1,30 +1,26 @@
 import React from 'react';
-import { SlSocialTwitter } from "react-icons/sl";
-import { BsLink45Deg, BsBoxArrowUp } from "react-icons/bs";
+import ReactDOM from "react-dom";
+import QRCode from "react-qr-code";
+// import { SlSocialTwitter } from "react-icons/sl";
+import { BsLink45Deg } from "react-icons/bs";
 import { HiMail } from "react-icons/hi";
+import { FaShare } from "react-icons/fa";
 import { toast } from "react-toastify";
 import Link from "next/link";
 import Image from 'next/image';
 import Modal from "../Modal"
 
-// IMAGE ASSETS
-import ShQrCode from "../../assets/starHealth-share.jpg";
 
 const ShareIcons = () => {
   const [isOpen, setIsOpen] = React.useState(false);
 
+  let href = '';
+
   const copyURI = (evt: any) => {
     evt.preventDefault();
-
-    const parent = evt.target.parentElement
-
-    if (evt.target.hasAttribute('href')) {
-      navigator.clipboard.writeText(evt.target.getAttribute('href'))
-    } else {
-      navigator.clipboard.writeText(parent.getAttribute('href'))
-    }
-
-    toast('Link https://www.starhealth.io/ added to clipboard!', { hideProgressBar: true, autoClose: 2000, type: 'success' })
+    
+    navigator.clipboard.writeText(window.location.href)
+    toast(`Link ${window.location.href} added to clipboard!`, { hideProgressBar: true, autoClose: 2000, type: 'success' })
   }
 
   // const generalShare = async (evt: any) => {
@@ -43,12 +39,17 @@ const ShareIcons = () => {
   //   }
   // }
 
+  if (typeof window !== "undefined") {
+    href = window?.location?.href;
+  }
+
   return (
     <>
       <button
-        className="text-white underline"
+        className="text-white underline flex p-1"
         onClick={() => setIsOpen(true)}>
           Share
+          <FaShare className='pt-1' size={20} color="white" />
       </button>
       <Modal
         open={isOpen}
@@ -56,7 +57,13 @@ const ShareIcons = () => {
         message="Share"
       >
         <div className='share-icons flex-col w-3/4 m-auto'>
-          <Image src={ShQrCode} alt="Star Health QR Code" className='w-full' />
+          <QRCode
+            style={{ height: "auto", maxWidth: "100%", width: "100%" }}
+            value={href}
+            viewBox={`0 0 300 300`}
+            fgColor={'#6d28d9'}
+          />
+          {/* <Image src={ShQrCode} alt="Star Health QR Code" className='w-full' /> */}
           <div className="footer-social-wrapper mt-5 flex w-full justify-between flex-col content-center">
               <button
                 className="ease w-full mx-auto leading-loose mt-2 focus:shadow-outline select-none rounded-md border border-violet-700 bg-violet-700 px-4 py-2 text-white transition duration-500 hover:bg-violet-900 focus:outline-none flex justify-center"
@@ -65,7 +72,7 @@ const ShareIcons = () => {
                 <BsLink45Deg className='mr-1' size={30} color="white" />
                 Copy Embed
               </button>
-              <Link href="mailto:">
+              <Link href={`mailto:?subject=Star Health Data&body=Please take a look at this healthcare data from Star Health at the following link ${href}`} rel="noopener noreferrer">
                 <button
                   className="ease w-full mx-auto mt-2 leading-loose focus:shadow-outline select-none rounded-md border border-violet-700 bg-violet-700 px-4 py-2 text-white transition duration-500 hover:bg-violet-900 focus:outline-none flex justify-center"
                 >
@@ -73,7 +80,7 @@ const ShareIcons = () => {
                   Email
                 </button>
               </Link>
-              <Link className="twitter-share-button" href="https://twitter.com/intent/tweet?text=Find%20Data-driven%20Health%20Care%20at%20https://www.starhealth.io/" data-size="large" target="_blank" rel="noreferrer">
+              {/* <Link className="twitter-share-button" href="https://twitter.com/intent/tweet?text=Find%20Data-driven%20Health%20Care%20at%20https://www.starhealth.io/" data-size="large" target="_blank" rel="noreferrer">
                 <button
                   className="ease w-full mx-auto mt-2 leading-loose focus:shadow-outline select-none rounded-md border border-violet-700 bg-violet-700 px-4 py-2 text-white transition duration-500 hover:bg-violet-900 focus:outline-none flex justify-center"
                   onClick={copyURI}
@@ -81,7 +88,7 @@ const ShareIcons = () => {
                   <SlSocialTwitter className='mr-2' size={26} color="white" />
                   Share
                 </button>
-              </Link>
+              </Link> */}
           </div>
         </div>
       </Modal>
