c9604d2c00abd52cf55946ce4d3e83059c5f9f9e
Nakassony Bernardo <nakassony.bernardo@tistech.co.ao>
2023-07-29 05:56:28 +0100

    feat: Graph Generation from API data

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,6 +11,7 @@ import { trpc } from "../utils/trpc";
 import "../styles/globals.css";
 import { NavFoot } from "../components/NavFoot";
 import Head from "next/head";
+import { chown } from "fs";
 
 {
   /* <React.StrictMode>
@@ -66,50 +67,77 @@ const MyApp: AppType<{ session: Session | null }> = ({
   
   function initialLoad(){
     const urlDisease = `${baseUrl}/genetics/condition/15q24-microdeletion`;
-    const urlGenes = `${baseUrl}/genetics/chromosome/$id`;
+    
+    httpRequest(urlDisease)
+    .then(async (result) => {
+      console.log(`Using promise now`);
+      let nodeId = 0;
 
-    httpRequest(urlDisease, parseDisease);
+      let disease = JSON.parse(result);
+      disease = disease?.condition;
+      const geneId = disease['related-gene-list'][0]['related-gene']['gene-symbol'];
+      const urlGenes = `${baseUrl}/genetics/chromosome/${geneId}`;
+      
+      let geneRequest = await httpRequest(urlGenes);
+      geneRequest = JSON.parse(geneRequest);
+      const chromosomes = geneRequest['chromosome']['chromosome-summary']['related-health-condition-list']['related-health-condition']
+      
+      const chromosomeNodes = [...chromosomes].map(chromo => (
+        { id: ++nodeId, label: `${chromo?.name?._text}`, group: 0 }
+      ))
+
+      const chromosomeEdges = [...chromosomes].map(chromo => (
+        { from: nodeId--, to: 0 }
+      ));
+
+      const nodes = [{ id: 0, label: `${disease?.name}`, group: 0 }, ...chromosomeNodes];
+      
+      // create a network
+      const container = document.getElementById("graphPlaceholder");
+      const data = { nodes: nodes, edges: chromosomeEdges,};
+  
+      const options = {
+        nodes: {
+          shape: "dot",
+          size: 16,
+          font: {
+            color: 'white'
+          }
+        },
+        physics: {
+          forceAtlas2Based: {
+            gravitationalConstant: -26,
+            centralGravity: 0.005,
+            springLength: 230,
+            springConstant: 0.18,
+          },
+          maxVelocity: 146,
+          solver: "forceAtlas2Based",
+          timestep: 0.35,
+          stabilization: { iterations: 150 },
+        },
+      };
+  
+      new vis.Network(container, data, options);
+
+    });
     
   }
 
-  function parseDisease(diseaseData){
-    console.log(`Load from parse Disease`);
-    
-    let disease = JSON.parse(diseaseData);
-    disease = disease?.condition;
-    const geneId = disease['related-gene-list'][0]['related-gene']['gene-symbol'];
+  function httpRequest(url){
 
-    const nodes = [{ id: 0, label: `${disease?.name}`, group: 0 }];
-    
-    const edges = [/* { from: 1, to: 0 } */];
-    
-    // create a network
-    const container = document.getElementById("graphPlaceholder");
-    const data = { nodes: nodes, edges: edges,};
+    return new Promise((resolve, reject) => {
 
-    const options = {
-      nodes: {
-        shape: "dot", size: 30,
-        font: { size: 32, color: "#ffffff",},
-        borderWidth: 2,
-      },
-      edges: { width: 2,},
-    };
-
-    new vis.Network(container, data, options);
-     
-  }
-
-  function httpRequest(url, callback){
-
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET',url,true);
-    xhr.send();
-    xhr.onreadystatechange = function(){
-      if(xhr.readyState == 4){
-        callback(xhr.responseText)
+      const xhr = new XMLHttpRequest();
+      xhr.open('GET',url,true);
+      xhr.send();
+      xhr.onreadystatechange = function(){
+        if(xhr.readyState == 4){
+          resolve(xhr.responseText)
+        }
       }
-    }
+
+    })
 
   }
 
