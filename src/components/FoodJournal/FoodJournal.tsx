2f798ebd76c63a9746cde4a683567cf225f6e511
YoussefBarsoom <youssefbarsoom@gmail.com>
2023-08-15 00:09:11 -0400

    Add Loading

--- src/components/FoodJournal/FoodJournal.tsx
+++ src/components/FoodJournal/FoodJournal.tsx
@@ -10,6 +10,7 @@ import { AiOutlineClose, AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 import { BsChevronCompactRight } from "react-icons/bs";
 import {MdWaterDrop} from 'react-icons/md'
 import { set, update } from "lodash";
+import { toast } from "react-toastify";
 
 const Modal = ({
   open,
@@ -61,7 +62,12 @@ const FoodJournal: React.FC = () => {
     const [mealCategory,setMealCategory]=useState(0)
     const [selectedID,setSelectedID]=useState()
     const [waterIntake,setWaterIntake]=useState(0)
-    const [waterIntakeBtn,setWaterIntakeBtn] =useState<boolean>(false)
+    const [waterIntakeBtn,setWaterIntakeBtn] =useState<number>(1)
+    const [gettingDataForAdd,setGettingDataForAdd] = useState<boolean>(false)
+    // Number Status
+    // 1 Ready
+    // 2 Loading
+    // 3 Done
     function waterIntakeComp()
     {
       const icons = [];
@@ -165,7 +171,7 @@ const FoodJournal: React.FC = () => {
           date:foodJournalDate,
           waterIntake:waterIntake
         }
-        setWaterIntakeBtn(true)
+        setWaterIntakeBtn(2)
         try{
           
           fetch("/api/foodJournal/postWaterIntake", {
@@ -174,8 +180,10 @@ const FoodJournal: React.FC = () => {
               "Content-Type": "application/json",
             },
             body: JSON.stringify(body)}).then((message:any)=>{
-            setWaterIntakeBtn(false)
-
+            setWaterIntakeBtn(3)
+            setTimeout(() => {
+              setWaterIntakeBtn(1)
+            }, 2000);
             }
             )
           }
@@ -257,7 +265,9 @@ const FoodJournal: React.FC = () => {
       useEffect(()=>{
         if (currentFood)
         {
-          
+        //disable button 
+        //add loading  
+        setGettingDataForAdd(true)
         const delayDebounceFn = setTimeout(() => {
         fetch("/api/food/"+currentFood.id).then((response) => {
         response.json().then((data) => {
@@ -272,6 +282,7 @@ const FoodJournal: React.FC = () => {
         })
         setSelectedPortion(0)
         setPortionOptions(output)
+        setGettingDataForAdd(false)
 
         });
     })}, 250);
@@ -312,8 +323,13 @@ const submitFood = async (e: React.FormEvent) =>{
         "Content-Type": "application/json",
       },
       body: JSON.stringify(body)}).then((message:any)=>{
-        setAddStatus(3)
-
+        toast(`Successfully added Food item.`, {})
+        setOpenFoodModal(false);
+        setAddStatus(1);
+        setNumOfServings(1);
+        setCurrentFood('');
+        setSearchStr('');
+        setMealCategory(0)
       }
       )
     }
@@ -388,7 +404,7 @@ const submitFood = async (e: React.FormEvent) =>{
                 <input value={mealDate} onChange={(e)=>{setMealDate(e.target.value)}} className="rounded-lg border border-violet-900 bg-violet-100 p-1 text-slate-900 placeholder:text-violet-800 hover:bg-violet-300 hover:text-violet-900" type="date"></input>
             </div>
             <div>
-                {addStatus==1&&<button onClick={(e)=>{setAddStatus(2);submitFood(e);}} className="w-[100%] ease focus:shadow-outline select-none rounded-md border border-violet-700 bg-violet-700 px-4 py-2 text-white transition duration-500 hover:bg-violet-900 focus:outline-none">
+                {addStatus==1&&<button onClick={(e)=>{if(!gettingDataForAdd){setAddStatus(2);submitFood(e);}}} className="w-[100%] ease focus:shadow-outline select-none rounded-md border border-violet-700 bg-violet-700 px-4 py-2 text-white transition duration-500 hover:bg-violet-900 focus:outline-none">
                     Add Meal 
                 </button>}
                 {addStatus==3&&
@@ -403,7 +419,28 @@ const submitFood = async (e: React.FormEvent) =>{
                 Food Nutrients
             </div>
             <div className="foodNutrientsSection">
-            {currentFood&&foodNutrients(currentFoodDetails)}
+            {gettingDataForAdd&&<>
+
+              <div className="mx-auto flex justify-center items-center h-[300px] w-[100%]">
+                <svg
+                  role="status"
+                  className="mr-2 inline h-20 w-20 animate-spin fill-purple-600 text-gray-200 dark:text-gray-600"
+                  viewBox="0 0 100 101"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                    fill="currentColor"
+                  />
+                  <path
+                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                    fill="currentFill"
+                  />
+                </svg>
+              </div>
+            </>}
+            {!gettingDataForAdd&&currentFood&&foodNutrients(currentFoodDetails)}
             </div>
         </div>
         </div>}
@@ -545,9 +582,20 @@ const submitFood = async (e: React.FormEvent) =>{
             </div>
             <button 
         className="w-[100%] ease focus:shadow-outline select-none rounded-md border border-[#21b6f5] bg-[#21b6f5] px-4 py-2 text-white transition duration-500 hover:bg-[#098ac1] focus:outline-none"
-        disabled={waterIntakeBtn}
+        disabled={waterIntakeBtn==2}
         onClick={(e)=>{submitWaterIntake()}}>
         Save Water Intake
+        {waterIntakeBtn==2&&
+        <>
+<div className="lds-ring"><div></div><div></div><div></div><div></div></div>
+        </>}
+        {waterIntakeBtn==3&&
+        <>
+        										<svg className="h-5 w-5 inline-block mr-2 -mt-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+											<path d="M5 13l4 4L19 7"></path>
+										</svg>
+        </>}
+        
         </button>
           </div>
         </div>}
